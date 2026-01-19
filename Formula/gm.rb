6717ff0a3aaf9c7e93726daf95120a2fc3551b96
Nelson Melo <nmelo@users.noreply.github.com>
2026-01-19 17:48:05 -0500

    Update gm to v0.2.1 - fix identity detection to use tmux window name

--- Formula/gm.rb
+++ Formula/gm.rb
@@ -1,28 +1,28 @@
 class Gm < Formula
   desc "Agent-to-agent messaging CLI using beads"
   homepage "https://github.com/nmelo/gasmail"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nmelo/gasmail/releases/download/v0.2.0/gasmail_darwin_arm64.tar.gz"
-      sha256 "d2d48491e76d5b926222efd9971137cc629bc691e3ea2a8570fd9427d969bdba"
+      url "https://github.com/nmelo/gasmail/releases/download/v0.2.1/gasmail_darwin_arm64.tar.gz"
+      sha256 "575cd2b88b0f880dea39c6d2f3b918959bb5312358b24ef6701b2c89001f46ab"
     end
     on_intel do
-      url "https://github.com/nmelo/gasmail/releases/download/v0.2.0/gasmail_darwin_amd64.tar.gz"
-      sha256 "6bcf2d43ace8eb65ecf88726ed43831b4586d7f0c09a7e6c15dabee6035833ac"
+      url "https://github.com/nmelo/gasmail/releases/download/v0.2.1/gasmail_darwin_amd64.tar.gz"
+      sha256 "ae39f1ba79e0506523220271a22857ee4e52e275edb145750bc5cad6dbb29d59"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nmelo/gasmail/releases/download/v0.2.0/gasmail_linux_arm64.tar.gz"
-      sha256 "9fb319f082e1b1c298085e2c08ed8db76df9fe2b253fb0493da0240b15697c06"
+      url "https://github.com/nmelo/gasmail/releases/download/v0.2.1/gasmail_linux_arm64.tar.gz"
+      sha256 "f71e2509aed75a7d8511ad07698a6a5b8a3a2205e57bbfc3798e768b5b7be36b"
     end
     on_intel do
-      url "https://github.com/nmelo/gasmail/releases/download/v0.2.0/gasmail_linux_amd64.tar.gz"
-      sha256 "114e9d16af3e84bfe53bc621b31eae2546722dbcc7aac1fae4d36c07304cce58"
+      url "https://github.com/nmelo/gasmail/releases/download/v0.2.1/gasmail_linux_amd64.tar.gz"
+      sha256 "90c9dc4fd43bd89571b5e13c319c90624315f10e2d48c51373f89fc840e81547"
     end
   end
 
