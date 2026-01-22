27eafe700782000ce32721c33bea357b52be2b1b
Nelson Melo <nmelo@users.noreply.github.com>
2026-01-22 14:47:59 -0500

    beads-ui 0.2.1

--- Formula/beads-ui.rb
+++ Formula/beads-ui.rb
@@ -1,11 +1,11 @@
 class BeadsUi < Formula
   desc "Web interface for viewing and managing beads issue tracking databases"
   homepage "https://github.com/nmelo/bdui"
-  version "0.2.0"
+  version "0.2.1"
   license "Apache-2.0"
 
-  url "https://github.com/nmelo/bdui/releases/download/v0.2.0/beads-ui-0.2.0-standalone.tar.gz"
-  sha256 "cd5677a39ef5fcda8833607cbdd982a1bd7449cf7895e351c13a9be55b606595"
+  url "https://github.com/nmelo/bdui/releases/download/v0.2.1/beads-ui-0.2.1-standalone.tar.gz"
+  sha256 "27c1ef43d869f364a72313b6a5e0ed44be872079c34b37f5aa07c586a7716ac9"
 
   depends_on "node"
 
