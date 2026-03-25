794717491e30868c099a2091e400d6b18bec8108
Nelson Melo <238738+nmelo@users.noreply.github.com>
2026-03-24 22:06:00 -0400

    initech: bump to v0.2.0

--- Formula/initech.rb
+++ Formula/initech.rb
@@ -1,28 +1,28 @@
 class Initech < Formula
   desc "Bootstrap and manage multi-agent development projects"
   homepage "https://github.com/nmelo/initech"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nmelo/initech/releases/download/v0.1.0/initech_darwin_arm64.tar.gz"
-      sha256 "f5c7f0a5a006b2ede1a22db1b80b53c06a072eba0090c0f6fee64d06bd9df904"
+      url "https://github.com/nmelo/initech/releases/download/v0.2.0/initech_darwin_arm64.tar.gz"
+      sha256 "615b1336fdba756a5af802452af1ea95e92309bddde4833a26b566c38c3c98b0"
     end
     on_intel do
-      url "https://github.com/nmelo/initech/releases/download/v0.1.0/initech_darwin_amd64.tar.gz"
-      sha256 "16fd6c37f32bdf48bf6f26803a3b32ec2e07381b69abb0e0a9cf42678cb82969"
+      url "https://github.com/nmelo/initech/releases/download/v0.2.0/initech_darwin_amd64.tar.gz"
+      sha256 "94e694ecf813841a206035a666ef5f8b0263bd6bfd9b2239fbf8a33ce059414c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/nmelo/initech/releases/download/v0.1.0/initech_linux_arm64.tar.gz"
-      sha256 "2b24b1c97088b2a454bd8677963141d4fe1512f7ec8dc64c75824679156b4b4b"
+      url "https://github.com/nmelo/initech/releases/download/v0.2.0/initech_linux_arm64.tar.gz"
+      sha256 "b99ef38f4db4dcd817f16f804561ad8e892d60c928785aa3c630ae30ad16af94"
     end
     on_intel do
-      url "https://github.com/nmelo/initech/releases/download/v0.1.0/initech_linux_amd64.tar.gz"
-      sha256 "96fde82104a544c1c47bd7a1086fa7d9d0620d76cf3a1b75e883f676212fec67"
+      url "https://github.com/nmelo/initech/releases/download/v0.2.0/initech_linux_amd64.tar.gz"
+      sha256 "f7c0810ded0adf59407ebe0c72be7975c5db55406ff4f83285bf722ba75115b2"
     end
   end
 
